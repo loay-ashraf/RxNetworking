5fab681204f052a084575d2b93cab1103cc66f81
Loay Ashraf <loay.ashraf.96@gmail.com>
2024-10-01 12:19:12 +0300

    feat: update documentation
    Resolves: none.

--- Source/HTTP/Extensions/Data+Append.swift
+++ Source/HTTP/Extensions/Data+Append.swift
@@ -7,8 +7,19 @@
 
 import Foundation
 
+/// An extension to the `Data` class that provides additional functionalities
+/// for appending multiple `Data` objects to an existing `Data` instance.
 extension Data {
+    
+    /// Appends the contents of an array of `Data` objects to the current `Data` instance.
+    ///
+    /// This method iterates through the provided array and appends each `Data` object
+    /// to the receiver.
+    /// 
+    /// - Parameter data: An array of `Data` objects to append to the current instance.
     mutating func append(contentsOf data: [Data]) {
         data.forEach({ self.append($0) })
     }
+    
 }
+
--- Source/HTTP/Extensions/String+InputStream.swift
+++ Source/HTTP/Extensions/String+InputStream.swift
@@ -7,9 +7,19 @@
 
 import Foundation
 
+/// An extension to the `String` class providing additional functionalities,
+/// specifically for converting a string into an `InputStream`.
 extension String {
+    
+    /// Converts the string into an `InputStream`.
+    ///
+    /// This property creates an `InputStream` from the string's UTF-8 encoded data.
+    /// If the conversion fails, it returns `nil`.
+    ///
+    /// - Returns: An optional `InputStream` created from the string's data.
     var inputStream: InputStream? {
         guard let data = self.data(using: .utf8) else { return nil }
         return .init(data: data)
     }
+    
 }
